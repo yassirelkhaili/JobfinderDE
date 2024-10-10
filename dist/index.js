f92538c1a1e5a0d024e854ac48349c390f5c91b7
Yassir Elkhaili <y.elkhaili@ephilos.lokal>
2024-10-10 11:35:29 +0200

    random changes

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,34 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const puppeteer_1 = __importDefault(require("puppeteer"));
+const config_1 = require("./config/config");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-console.log(process.env.GPT_API_KEY);
+function scrapeJobs(arbeitsBezeichnungen) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const browser = yield puppeteer_1.default.launch({ headless: true });
+        const page = yield browser.newPage();
+        yield page.goto(arbeitsBezeichnungen.url, { waitUntil: 'networkidle2' });
+        const jobs = yield page.evaluate(() => {
+        });
+        yield browser.close();
+        return jobs;
+    });
+}
+const displayJobs = (jobs) => console.table(jobs);
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    const jobs = yield scrapeJobs(config_1.jobSuchKonfiguration);
+    displayJobs(jobs);
+}))();
