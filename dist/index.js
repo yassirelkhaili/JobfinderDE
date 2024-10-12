0697593d6dc57be73e83537934cdd75bac7c56aa
yassirelkhaili <elkhailiyassir@gmail.com>
2024-10-12 10:37:43 +0200

    saving progress

--- dist/index.js
+++ dist/index.js
@@ -19,44 +19,44 @@ async function scrapeJobs(config) {
         browser = await puppeteer_1.default.launch({ headless: false });
         const page = await browser.newPage();
         // Öffnet die URL und wartet, bis die Seite vollständig geladen ist
-        await page.goto(config.url, { waitUntil: 'networkidle2' });
+        await page.goto(config.url, { waitUntil: "networkidle2" });
         const cookies = await page.cookies();
         await page.deleteCookie(...cookies);
         await page.reload();
         // Parse the values for arbeitsBezeichnungen and ort
-        const arbeitsBezeichnungen = helperService_1.default.parseArbeitsBezeichnungen(config['arbeitsBezeichnungen']);
-        const ort = config['ort'];
+        const arbeitsBezeichnungen = helperService_1.default.parseArbeitsBezeichnungen(config["arbeitsBezeichnungen"]);
+        const ort = config["ort"];
         // Führt die Evaluierungsfunktion auf der Seite aus
         /**
-        * Sucht nach dem `bahf-cookie-disclaimer-dpl3`-Element, greift auf sein `shadowRoot` zu,
-        * und klickt auf den Button `.ba-btn-contrast`, um die Cookie-Bestätigung abzuschließen, falls vorhanden.
-        */
+         * Sucht nach dem `bahf-cookie-disclaimer-dpl3`-Element, greift auf sein `shadowRoot` zu,
+         * und klickt auf den Button `.ba-btn-contrast`, um die Cookie-Bestätigung abzuschließen, falls vorhanden.
+         */
         await page.evaluate(async () => {
-            await new Promise(resolve => setTimeout(resolve, 100));
-            const rootElement = document.querySelector('bahf-cookie-disclaimer-dpl3');
+            await new Promise((resolve) => setTimeout(resolve, 100));
+            const rootElement = document.querySelector("bahf-cookie-disclaimer-dpl3");
             if (rootElement && rootElement.shadowRoot) {
-                const cookieButton = rootElement.shadowRoot.querySelector('.ba-btn-contrast');
+                const cookieButton = rootElement.shadowRoot.querySelector(".ba-btn-contrast");
                 if (cookieButton) {
                     cookieButton.click();
-                    await new Promise(resolve => setTimeout(resolve, 1000));
+                    await new Promise((resolve) => setTimeout(resolve, 1000));
                 }
             }
         });
         /**
-     * Füllt das erste Eingabefeld mit der Arbeitsbezeichnung und das zweite mit dem Ort aus,
-     * und klickt anschließend auf den Submit-Button.
-     */
-        await page.type('#was-input', arbeitsBezeichnungen);
-        await page.type('#wo-input', ort);
-        await page.click('#btn-stellen-finden');
-        await page.waitForSelector('#ergebnis-container', { timeout: 10000 });
-        await page.click('#filter-toggle');
+         * Füllt das erste Eingabefeld mit der Arbeitsbezeichnung und das zweite mit dem Ort aus,
+         * und klickt anschließend auf den Submit-Button.
+         */
+        await page.type("#was-input", arbeitsBezeichnungen);
+        await page.type("#wo-input", ort);
+        await page.click("#btn-stellen-finden");
+        await page.waitForSelector("#ergebnis-container", { timeout: 10000 });
+        await page.click("#filter-toggle");
         await new Promise((resolve) => setTimeout(resolve, 1000));
         const jobSuchKonfigurationsArray = Object.entries(config_1.jobSuchKonfiguration);
         for (const [key] of jobSuchKonfigurationsArray.slice(3)) {
             try {
                 const dropDownButtonSelector = `button[id="${key}-accordion-heading-link"]`;
-                const isCollapsed = await page.$eval(dropDownButtonSelector, dropDownButton => dropDownButton.classList.contains('collapsed'));
+                const isCollapsed = await page.$eval(dropDownButtonSelector, (dropDownButton) => dropDownButton.classList.contains("collapsed"));
                 if (isCollapsed)
                     await page.click(dropDownButtonSelector);
             }
@@ -81,11 +81,17 @@ async function scrapeJobs(config) {
                 console.warn("Value parsing failed or value missing in config.");
             }
         }
-        await page.click('#footer-button-modales-slide-in-filter');
+        await page.click("#footer-button-modales-slide-in-filter");
         await new Promise((resolve) => setTimeout(resolve, 100));
-        do {
-            await page.click('#ergebnisliste-ladeweitere-button');
-        } while (await page.$('#ergebnisliste-ladeweitere-button') !== null);
+        async function clickButtonUntilGone(page) {
+            const button = await page.$('#ergebnisliste-ladeweitere-button');
+            if (button === null) {
+                return;
+            }
+            await button.click();
+            await clickButtonUntilGone(page);
+        }
+        await clickButtonUntilGone(page);
         // const offerNavigationElements = await page.$$('.your-class-name');
     }
     catch (error) {
--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import puppeteer from "puppeteer";
 import { jobSuchKonfiguration } from "./config/config";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import helperService from "./services/helperService";
 import type { Job, JobSuchKonfiguration } from "./types/types";
+import type { Page } from "puppeteer";
 
 dotenv.config();
 
@@ -14,95 +15,118 @@ dotenv.config();
 async function scrapeJobs(config: JobSuchKonfiguration): Promise<void> {
   let browser; // intiate browser instance
   try {
-  browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
+    browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
 
-  // Öffnet die URL und wartet, bis die Seite vollständig geladen ist
-  await page.goto(config.url, { waitUntil: 'networkidle2' });
+    // Öffnet die URL und wartet, bis die Seite vollständig geladen ist
+    await page.goto(config.url, { waitUntil: "networkidle2" });
 
-  const cookies = await page.cookies();
-  await page.deleteCookie(...cookies);
-  await page.reload();
+    const cookies = await page.cookies();
+    await page.deleteCookie(...cookies);
+    await page.reload();
 
-  // Parse the values for arbeitsBezeichnungen and ort
-  const arbeitsBezeichnungen = helperService.parseArbeitsBezeichnungen(config['arbeitsBezeichnungen']);
-  const ort = config['ort'];
+    // Parse the values for arbeitsBezeichnungen and ort
+    const arbeitsBezeichnungen = helperService.parseArbeitsBezeichnungen(
+      config["arbeitsBezeichnungen"]
+    );
+    const ort = config["ort"];
 
-  // Führt die Evaluierungsfunktion auf der Seite aus
-  /**
-  * Sucht nach dem `bahf-cookie-disclaimer-dpl3`-Element, greift auf sein `shadowRoot` zu,
-  * und klickt auf den Button `.ba-btn-contrast`, um die Cookie-Bestätigung abzuschließen, falls vorhanden.
-  */
-  await page.evaluate(async () => {
-    await new Promise(resolve => setTimeout(resolve, 100));
-    const rootElement = document.querySelector('bahf-cookie-disclaimer-dpl3');
-    if (rootElement && rootElement.shadowRoot) {
-      const cookieButton = rootElement.shadowRoot.querySelector('.ba-btn-contrast') as HTMLButtonElement | null;
-      if (cookieButton) {
-        cookieButton.click();
-        await new Promise(resolve => setTimeout(resolve, 1000));
+    // Führt die Evaluierungsfunktion auf der Seite aus
+    /**
+     * Sucht nach dem `bahf-cookie-disclaimer-dpl3`-Element, greift auf sein `shadowRoot` zu,
+     * und klickt auf den Button `.ba-btn-contrast`, um die Cookie-Bestätigung abzuschließen, falls vorhanden.
+     */
+    await page.evaluate(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 100));
+      const rootElement = document.querySelector("bahf-cookie-disclaimer-dpl3");
+      if (rootElement && rootElement.shadowRoot) {
+        const cookieButton = rootElement.shadowRoot.querySelector(
+          ".ba-btn-contrast"
+        ) as HTMLButtonElement | null;
+        if (cookieButton) {
+          cookieButton.click();
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+        }
+      }
+    });
+
+    /**
+     * Füllt das erste Eingabefeld mit der Arbeitsbezeichnung und das zweite mit dem Ort aus,
+     * und klickt anschließend auf den Submit-Button.
+     */
+    await page.type("#was-input", arbeitsBezeichnungen);
+    await page.type("#wo-input", ort);
+    await page.click("#btn-stellen-finden");
+
+    await page.waitForSelector("#ergebnis-container", { timeout: 10000 });
+
+    await page.click("#filter-toggle");
+
+    await new Promise((resolve: (value?: unknown) => void) =>
+      setTimeout(resolve, 1000)
+    );
+
+    const jobSuchKonfigurationsArray = Object.entries(jobSuchKonfiguration) as [
+      keyof JobSuchKonfiguration,
+      JobSuchKonfiguration[keyof JobSuchKonfiguration]
+    ][];
+
+    for (const [key] of jobSuchKonfigurationsArray.slice(3)) {
+      try {
+        const dropDownButtonSelector: string = `button[id="${key}-accordion-heading-link"]`;
+        const isCollapsed: boolean = await page.$eval(
+          dropDownButtonSelector,
+          (dropDownButton) => dropDownButton.classList.contains("collapsed")
+        );
+        if (isCollapsed) await page.click(dropDownButtonSelector);
+      } catch (error: any) {
+        console.warn(`Error has occurred: ${error.message}`);
       }
     }
-  });
 
-      /**
-   * Füllt das erste Eingabefeld mit der Arbeitsbezeichnung und das zweite mit dem Ort aus,
-   * und klickt anschließend auf den Submit-Button.
-   */
-      await page.type('#was-input', arbeitsBezeichnungen);
-      await page.type('#wo-input', ort);
-      await page.click('#btn-stellen-finden');
-
-      await page.waitForSelector('#ergebnis-container', { timeout: 10000 });
-
-      await page.click('#filter-toggle');
-
-      await new Promise((resolve: (value?: unknown) => void) => setTimeout(resolve, 1000));
-
-      const jobSuchKonfigurationsArray = Object.entries(jobSuchKonfiguration) as [keyof JobSuchKonfiguration, JobSuchKonfiguration[keyof JobSuchKonfiguration]][];
-
-      for (const [key] of jobSuchKonfigurationsArray.slice(3)) {
-        try {
-          const dropDownButtonSelector: string = `button[id="${key}-accordion-heading-link"]`;
-          const isCollapsed: boolean = await page.$eval(dropDownButtonSelector, dropDownButton => dropDownButton.classList.contains('collapsed'));
-          if (isCollapsed) await page.click(dropDownButtonSelector);
-        } catch(error: any) {
-          console.warn(`Error has occurred: ${error.message}`);
-        }
-      }
-
-    await new Promise((resolve: (value?: unknown) => void) => setTimeout(resolve, 1000));
+    await new Promise((resolve: (value?: unknown) => void) =>
+      setTimeout(resolve, 1000)
+    );
 
     for (const [key, value] of jobSuchKonfigurationsArray.slice(3)) {
-          const bezeichnungen = helperService.parseInputIdSelector(value);
-          if (bezeichnungen && Array.isArray(bezeichnungen)) {
-            for (const bezeichnung of bezeichnungen) {
-              try {
-                await page.click(`input[id="${key}-${bezeichnung}"]`);
-              } catch(error: any) {
-                console.warn(`Error has occured: ${error.message}`);
-              }
-            }
-          } else {
-            console.warn("Value parsing failed or value missing in config.");
-          }    
+      const bezeichnungen = helperService.parseInputIdSelector(value);
+      if (bezeichnungen && Array.isArray(bezeichnungen)) {
+        for (const bezeichnung of bezeichnungen) {
+          try {
+            await page.click(`input[id="${key}-${bezeichnung}"]`);
+          } catch (error: any) {
+            console.warn(`Error has occured: ${error.message}`);
+          }
         }
+      } else {
+        console.warn("Value parsing failed or value missing in config.");
+      }
+    }
 
-      await page.click('#footer-button-modales-slide-in-filter');
+    await page.click("#footer-button-modales-slide-in-filter");
 
-      await new Promise((resolve: (value?: unknown) => void) => setTimeout(resolve, 100));
+    await new Promise((resolve: (value?: unknown) => void) =>
+      setTimeout(resolve, 100)
+    );
 
-      do {
-        await page.click('#ergebnisliste-ladeweitere-button');
-        await new Promise(resolve => setTimeout(resolve, 800));
-      } while (await page.$('#ergebnisliste-ladeweitere-button') !== null);
+    async function clickButtonUntilGone(page: Page) {
+      const button = await page.$('#ergebnisliste-ladeweitere-button');
+      if (button === null) {
+        return;
+      }
+      await button.click();
+      await page.waitForSelector('', )
+      await clickButtonUntilGone(page);
+    }
+    
+    await clickButtonUntilGone(page);
 
-      // const offerNavigationElements = await page.$$('.your-class-name');
-} catch (error: any) {
-  console.warn(`Error has occured: ${error.message}`);
-} finally {
-  // if (browser) await browser.close();
-}
+    // const offerNavigationElements = await page.$$('.your-class-name');
+  } catch (error: any) {
+    console.warn(`Error has occured: ${error.message}`);
+  } finally {
+    // if (browser) await browser.close();
+  }
 }
 
 /**
@@ -114,4 +138,4 @@ const displayJobs = (jobs: Job[]) => console.table(jobs);
 (async () => {
   // Führt die Job-Scrape-Funktion aus und zeigt die Ergebnisse an
   await scrapeJobs(jobSuchKonfiguration);
-})()
+})();
